d37aaacf4734fe2ce85dbac805968cb1145f9b93
vratnoo <vratnoo@gmail.com>
2022-12-29 13:14:42 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+function Dummy({name}){
+  return(<>
+  <h2>{name}</h2>
+  </>)
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <h1>
+      Ratnoo is working
+    </h1>
+    <Dummy name={"vikram ratnoo"}/>
+   </div>    
   );
 }
 
