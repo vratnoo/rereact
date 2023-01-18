64f59005918f17c1b9c6d3a131fce23b32a6f15f
vratnoo <vratnoo@gmail.com>
2023-01-18 23:18:01 +0530

    added validation usig formik

--- src/feature/AddData.jsx
+++ src/feature/AddData.jsx
@@ -4,7 +4,7 @@ import { where,query,getFirestore,collection, addDoc, doc,getDocs,setDoc,deleteD
 import Context from '../auth/Store';
 import { toast } from 'react-hot-toast';
 import { useFormik } from 'formik';
-import * as Yup from 'yup'
+import * as Yup from 'yup';
 const villageCodes = [
     {'name':'No selected',code:0},
     {'name':'shiv',code:124554},
@@ -17,47 +17,41 @@ const initialState = {
     registrationId:"",
     landAccount:"",
     landId:"",
-    villageCode:{name:"",code:null}
-}
-
-const validate = values=>{
-    const errors = {}
-    if(!values.registrationId){
-        errors.registrationId = "Required"
-    }
-    if(!values.landAccount){
-        errors.landAccount  = "Required"
-    }
-    if(!values.landId){
-        errors.landId = "Required"
-    }
-    if(!values.villageCode.code==0){
-        errors.villageCode = "Please select village"
-    }
-    return errors
+    villageCode:{name:"No selected",code:0}
 }
 
 const AddData = ()=>{
-    const [landData,setLandData] = useState(initialState)
+    // const [landData,setLandData] = useState(initialState)
     const [data,setData] = useState(null)
     const [state,dispatch] = useContext(Context)
+
+    const validationSchema = Yup.object({
+      registrationId: Yup.string().min(5, "Registration id should be 5 charecter ").required(),
+      landAccount: Yup.number().required(),
+      landId: Yup.string().matches(/^(\d+\/\d+|\d+)$/, "value shoud be a valid khasra no  ").required("land id is required "),
+      villageCode: Yup.object({code: Yup.number().notOneOf([0], "Invalid selection")})
+    
+    })
+
+
     const formik = useFormik({
         initialValues:initialState,
-        validate,
+        validationSchema,
         onSubmit:(values)=>{
+            handleSubmit(values)
             console.log(values)
         },
     })
 
-    useEffect(() => {
-    fetch("data.json")
-      .then((response) => response.json())
-      .then((values) => setLandData((values)=>values));
-     console.log(data)
-  }, []);
+//     useEffect(() => {
+//     fetch("data.json")
+//       .then((response) => response.json())
+//       .then((values) => setLandData((values)=>values));
+//      console.log(data)
+//   }, []);
 
-    const handleSubmit = async(e)=>{
-        e.preventDefault()
+    const handleSubmit = async(landData)=>{
+        
         const user = state.user
         try {
             const dataRef = doc(collection(db,'landData'))
@@ -66,22 +60,20 @@ const AddData = ()=>{
                 toast.error('problem in submiting farmer details')
         }finally{
             toast.success('Farmer land data uploaded successfully.')
-            setLandData(initialState)
         }
 
 
     }
     const handleChange = (e)=>{
-        setLandData((oldState)=>{
+            
             if(e.target.name=='villageCode'){
-                const result = villageCodes.find((value,index)=>{
-                    return value.code==e.target.value
-                })
-                
-                return {...oldState,[e.target.name]:{name:result.name,code:e.target.value}}
+                console.log("here")
+                const result = villageCodes.find((village)=>village.code==e.target.value)
+                console.log(result)                
+                formik.setFieldValue('villageCode',result)
             }
-            return {...oldState,[e.target.name]:e.target.value}
-        })
+                
+        
     }
 
     return (
@@ -89,18 +81,21 @@ const AddData = ()=>{
             <p>Add form for data</p>
             <form action="" onSubmit={formik.handleSubmit}>
                 <label htmlFor="">Registration Id</label>
-                <input type="text"  name="registrationId" value={formik.values.registrationId}  onBlur={formik.handleBlur} onChange={formik.handleChange}/>
-                {formik.errors.registrationId ? <div>{formik.errors.registrationId}</div> : null}
+                <input type="text"  name="registrationId" value={formik.values.registrationId}  onBlur={formik.handleBlur}  onChange={formik.handleChange}/>
+                {formik.touched.registrationId && formik.errors.registrationId ? <div className='error'>{formik.errors.registrationId}</div> : null}
                 <label htmlFor="">Land Account (Khata no)</label>
                 <input type="text"  name="landAccount" value={formik.values.landAccount}  onBlur={formik.handleBlur} onChange={formik.handleChange} />
+                {formik.touched.landAccount && formik.errors.landAccount ? <div className='error'>{formik.errors.landAccount}</div> : null}
                 <label htmlFor="">Land Id (Khasra No)</label>
                 <input type="text"  name="landId"  value={formik.values.landId}  onBlur={formik.handleBlur} onChange={formik.handleChange} />
+                {formik.touched.landId && formik.errors.landId ? <div className='error'>{formik.errors.landId}</div> : null}
                 <label htmlFor="">Select Village</label>
-                <select name="villageCode" id="" value={formik.values.villageCode.code}  onBlur={formik.handleBlur} onChange={formik.handleChange}>
+                <select name="villageCode" id=""   onChange={handleChange}>
                 {villageCodes.map((village,index)=>{
                     return (<option key={index} value={village.code}>{village.name}</option> )
                 })}
                 </select>
+                {formik.touched.villageCode && formik.errors.villageCode ? <div className='error'>{formik.errors.villageCode.code}</div> : null}
                 <button type="submit">Submit</button>
             </form>
 
